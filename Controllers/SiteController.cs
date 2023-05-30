76de2d2a5ee238aad41237320fe49a12f8b8040d
TahirZamanKhan <tahirzamankhan2002@gmail.com>
2023-05-30 17:44:46 +0500

    Add project files.

---
+++ Controllers/SiteController.cs
@@ -0,0 +1,14 @@
+﻿using AdoDotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdoDotnet.Controllers
+{
+    public class SiteController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>(); 
+            return View(students);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdoDotnet.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+
+        [Display(Name ="Full Name:")]
+        [Required]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email:")]
+        [Required]
+        public string Email { get; set; }
+
+        [Display(Name = "Mobile No.:")]
+        [Required]
+        public string Mobile { get; set; }
+
+        [Display(Name = "Date of Birth:")]
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Notes")]
+        [Required]
+        public string Notes { get; set; }
+
+
+
+
+    }
+}
